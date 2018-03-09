72de80e502e72893561bdc98f2d047f453bc285c
Robert S <rsavian@nerdery.com>
2018-03-09 08:43:23 -0600

    change endpoint

--- src/routes/ApiRouter.ts
+++ src/routes/ApiRouter.ts
@@ -4,7 +4,7 @@ import * as request from 'request-promise-native';
 export class ApiRouter {
 
     constructor(router: express.Router) {
-        router.get('/api/starships/', this._starShipRoute.bind(this));
+        router.get('/api/starship/', this._starShipRoute.bind(this));
     }
 
     protected async _starShipRoute(req: express.Request, res: express.Response, next: express.NextFunction): Promise<void> {
